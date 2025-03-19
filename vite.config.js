65c23386d241e2005850abae9d61439793a7e9d2
neo <neolawrencemasilo@gmail.com>
2025-03-19 16:59:00 +0200

    implemented base url

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/https://github.com/lawrencemasilo/fhc-app.git',
   plugins: [
     tailwindcss(),
   ],
