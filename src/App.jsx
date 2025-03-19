9ed36f0cfd79cb50d3bd2fae1b6d8ee078edc897
neo <neolawrencemasilo@gmail.com>
2025-03-19 16:10:19 +0200

    implemented Inter font family

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { CompetitionList } from "./components/CompetitionList";
 function App() {
 
   return (
-    <div className="bg-white min-h-screen flex flex-col items-center">
+    <div className="bg-white min-h-screen flex flex-col items-center font-[Inter]">
       <CompetitionList />
     </div>
   )
